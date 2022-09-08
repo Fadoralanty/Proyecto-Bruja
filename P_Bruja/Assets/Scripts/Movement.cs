a53094ee4f27e6f8e2e6e9235e921f10155cffa5
Julian Lanty <julianemanueltejo@gmail.com>
2022-09-08 02:09:00 -0300

    ADDED  PLAYER MOVEMENT
    clase Movement.cs
    clase PlayerController.cs

---
+++ P_Bruja/Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] float _Speed;
+    Rigidbody2D _rb;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void Move(Vector2 dir)
+    {
+        _rb.velocity = (_Speed * Time.fixedDeltaTime * dir);
+    }
+}
