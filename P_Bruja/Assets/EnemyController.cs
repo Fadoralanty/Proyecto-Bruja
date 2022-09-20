ae13ee4c0af693877d5e09090fdac4b627221842
Julian Lanty <julianemanueltejo@gmail.com>
2022-09-20 01:30:09 -0300

    The Enemy Can die now

--- P_Bruja/Assets/EnemyController.cs
+++ P_Bruja/Assets/EnemyController.cs
@@ -1,8 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [SerializeField] private Transform _target;
+    private Movement _movement;
+    private Damageable _damageable;
+    private void Start()
+    {
+        _movement = GetComponent<Movement>();
+        _damageable = GetComponent<Damageable>();
+        _damageable.onDie.AddListener(OnDieListener);
+    }
     
+    
+
+    void OnDieListener()
+    {
+        Destroy(gameObject);
+    }
 }
