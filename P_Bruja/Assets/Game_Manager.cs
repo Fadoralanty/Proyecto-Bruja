e60a68ec1fea38f5dbf4f6048dd86764286170dd
Julian Lanty <julianemanueltejo@gmail.com>
2022-09-22 01:17:05 -0300

    creado prefabs INK DIALOGUE MANAGER.prefab, HUD.prefab
    creado escena JulianTejo INK moralidad.unity

---
+++ P_Bruja/Assets/Game_Manager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    public static Game_Manager instance;
+    [Range(-100, 100)] public int _morality;
+    private void Awake()
+    {
+        if (instance==null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+}
