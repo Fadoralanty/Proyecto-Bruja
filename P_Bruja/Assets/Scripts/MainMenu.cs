d1569804e2e75ef8993ad0d7522550e750ef316b
LonarCabello <104102202+LonarCabello@users.noreply.github.com>
2022-10-16 17:52:19 -0300

    Main Menu
    +Play Button
    +Credits Button
    +Exit Button
    +Scene of Menu

---
+++ P_Bruja/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField]
+    GameObject creditsScreen;
+
+    private void Start()
+    {
+        creditsScreen.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OpenOptions()
+    {
+        creditsScreen.SetActive(true);
+    }
+}
