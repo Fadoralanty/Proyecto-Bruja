08c5e3f2ce807fe92de1ddd32e8fb5572dc1dcf8
LonarCabello <104102202+LonarCabello@users.noreply.github.com>
2022-10-24 18:00:06 -0300

    Inventory Fix
    +Activate and Deactivate

--- P_Bruja/Assets/Scripts/PlayerController.cs
+++ P_Bruja/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     private Animator _anim;
     private InventorySimple inventory;
     [SerializeField] private UI_Inventory uiInventory;
+    private bool uiInventoryActive = true;
     private void Awake()
     {
         _movement = GetComponent<Movement>();
@@ -18,17 +19,33 @@ public class PlayerController : MonoBehaviour
         inventory = new InventorySimple();
         uiInventory.SetPlayer(this);
         uiInventory.SetInventory(inventory);  
+        uiInventory.DeactivateUI();
     }
 
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            uiInventoryActive = !uiInventoryActive;
+            ActivationUIInventory();
+        }
         float hor = Input.GetAxisRaw("Horizontal");
         float ver = Input.GetAxisRaw("Vertical");
         _moveDir = new Vector2(hor, ver);
         _anim.SetFloat("AnimVelX", hor);
         _anim.SetFloat("AnimVelY", ver);
     }
-
+    private void ActivationUIInventory()
+    {
+        if (uiInventoryActive == false)
+        {
+            uiInventory.ActivateUI();
+        }
+        if (uiInventoryActive == true)
+        {
+            uiInventory.DeactivateUI();
+        }
+    }
     private void FixedUpdate()
     {
         _movement.Move(_moveDir.normalized);
@@ -43,7 +60,7 @@ public class PlayerController : MonoBehaviour
         if(itemWorld != null)
         {
             //Touching Item
-            inventory.AddItem(itemWorld.GetItem());
+            inventory.AddItem(itemWorld.GetItem(), 19);
             itemWorld.DestroySelf();
         }
     }
